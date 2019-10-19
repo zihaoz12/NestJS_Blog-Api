dcc12cb7726af46dded93eba984c4ddd902bc07a
zihaoz12 <zzhaa12@gmail.com>
2019-10-18 17:56:32 -0700

    nestjs-mongoose-crud CRUD routes

--- src/posts/post.model.ts
+++ src/posts/post.model.ts
@@ -1,8 +1,12 @@
-import { prop } from '@typegoose/typegoose'
+import { prop } from '@typegoose/typegoose';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class Post{
+    @ApiModelProperty({ description: 'article title', example: 'Article 1'})
     @prop()
     title: string
+
+    @ApiModelProperty({ description: 'article title', example: 'Article 1'})
     @prop()
     content: string
 }
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Post, Body,Query, Param, Put, Delete } from '@nestjs/common';
-import { create } from 'domain';
-import { declareTypeAlias } from '@babel/types';
 import { ApiUseTags, ApiOperation, ApiModelProperty } from '@nestjs/swagger';
 import { Post as PostSchema} from './post.model';
 import { IsNotEmpty } from 'class-validator';
 import { InjectModel } from 'nestjs-typegoose';
 import { ModelType } from '@typegoose/typegoose/lib/types'
+
+
+import { Crud } from 'nestjs-mongoose-crud';
 
 //from Typescript
 class CreatePostDto{
@@ -16,52 +17,65 @@ class CreatePostDto{
     content: string
 }
 
-
+@Crud({
+    model: PostSchema,
+    routes: {
+        find: {
+            decorators: [
+                ApiOperation( { title: 'lists of article'})
+            ]
+        },
+        create: {
+            dto: CreatePostDto
+        }
+    },  
+})
 
 @Controller('posts')
 @ApiUseTags('Articles')
 export class PostsController {
     constructor(
-        @InjectModel(PostSchema) private readonly postModel: ModelType<PostSchema>
+        @InjectModel(PostSchema) private readonly model: ModelType<PostSchema>
     ){}
 
-    @Get()
-    @ApiOperation({ title: 'show blog articles'})
-    async index(){
-        return await this.postModel.find()
-    }
+    //no need if install nestjs-mongoose-crud
+    // @Get()
+    // @ApiOperation({ title: 'show blog articles'})
+    // async index(){
+    //     return await this.postModel.find()
+    // }
 
-    @Post()
-    @ApiOperation({ title: 'create post article' })
-    async create(@Body() CreatePostDto:CreatePostDto){
-        await this.postModel.create(CreatePostDto)
-        return {
-            success: true
-        }
-    }
+    // @Post()
+    // @ApiOperation({ title: 'create post article' })
+    // async create(@Body() CreatePostDto:CreatePostDto){
+    //     await this.postModel.create(CreatePostDto)
+    //     return {
+    //         success: true
+    //     }
+    // }
 
-    @Get(':id')
-    @ApiOperation({ title: 'article detail'})
-    async detail(@Param('id') id: string){
-        return await this.postModel.findById(id)
-    }
+    // @Get(':id')
+    // @ApiOperation({ title: 'article detail'})
+    // async detail(@Param('id') id: string){
+    //     return await this.postModel.findById(id)
+    // }
 
-    @Put(':id')
-    @ApiOperation({ title: 'edit article'})
-    async update(@Param('id') id: string, @Body() updatePostDto:CreatePostDto){
-        await this.postModel.findByIdAndUpdate(id, updatePostDto)
-        return{
-            success:true
-        }
-    }
+    // @Put(':id')
+    // @ApiOperation({ title: 'edit article'})
+    // async update(@Param('id') id: string, @Body() updatePostDto:CreatePostDto){
+    //     await this.postModel.findByIdAndUpdate(id, updatePostDto)
+    //     return{
+    //         success:true
+    //     }
+    // }
 
-    @Delete(':id')
-    @ApiOperation( {title: 'delete article'})
-    async remove(@Param('id') id: string){
-        await this.postModel.findByIdAndRemove(id)
-        return{
-            success:true
-        }
-    }
+    // @Delete(':id')
+    // @ApiOperation( {title: 'delete article'})
+    // async remove(@Param('id') id: string){
+    //     await this.postModel.findByIdAndRemove(id)
+    //     return{
+    //         success:true
+    //     }
+    // }
     
 }
